3f1a4a44e2620be96aabdff938833ea5c584eb2a
savariya <123457596+savaariya-29@users.noreply.github.com>
2024-09-25 21:35:27 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,34 @@
+from art import logo # type: ignore
+print(logo)
+
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid=0
+    for bidder in bidding_dictionary:
+        bid_amount=bidding_dictionary[bidder]
+        if bid_amount>highest_bid:
+            highest_bid=bid_amount
+            winner= bidder
+
+    print(f"The Winner is {winner} with a bid of ${highest_bid} ")
+
+
+
+bid={}
+
+continue_bidding=True
+while continue_bidding:
+    name = input("what is your name?:")
+    price = int(input("what is your bid?:"))
+    bid[name]=price
+    should_continue = input("Are there any other bidders type'yes' or 'no'.\n").lower()
+    if should_continue=="no":
+        continue_bidding=False
+        find_highest_bidder(bid)
+    elif should_continue=="yes":
+        print("\n"*20)
+
+
+
+
+
